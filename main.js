bbd54ba4845df13041299aa50d4c98d311f646b1
Daniel Arnaud <arnaudsb@hotmail.com>
2022-08-24 23:55:04 -0300

    JS onclick moved to .js file and used const for the first time

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom() {
+    document.querySelector("#som_tecla_pom").play();
 }
 
-
-document.querySelector('.tecla_pom');
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
