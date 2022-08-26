32bed7472f36318aa311b72a603d060ee25bc79e
Daniel Arnaud <arnaudsb@hotmail.com>
2022-08-26 00:44:20 -0300

    Criada função que faz cada tecla tocar um instrumento. String com crase e $

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function tocaSomPom() {
-    document.querySelector("#som_tecla_pom").play();
+const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeSons = document.querySelectorAll("audio");
+let contador = 0;
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    console.log (instrumento);
+
+    tecla.onclick = function () {  
+        tocaSom(idAudio); 
+    }
+
+
+    contador += 1;
+}
